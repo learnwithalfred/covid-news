c628f06f42241d54125375191d87147d6091e977
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-14 19:39:07 +0000

    Install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function App() {
-  return <div style={{ fontSize: 24 }}>App js</div>;
+  return (
+    <>
+      <h1>header goes here</h1>
+      <div style={{ fontSize: 24 }}>App js</div>
+      <button type="button" className="btn btn-primary">
+        Click me
+      </button>
+    </>
+  );
 }
 
 export default App;
