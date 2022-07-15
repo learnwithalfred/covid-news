f3ec29f59c86758c997db5ed7a765cdf09853a21
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-15 10:41:42 +0100

    Updated home page link

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 // import App from './App';
-import Home from './routes/home';
+// import Home from './routes/home';
 import DetailsPage from './routes/details';
 import Login from './features/auth/login';
+import HomePage from './features/metrics/HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Home />} />
+      <Route path="/home" element={<HomePage />} />
       <Route path="/details" element={<DetailsPage />} />
     </Routes>
   </BrowserRouter>,
