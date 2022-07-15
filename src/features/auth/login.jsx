21db8bb3e68f7436da2e351f8f5a40ee570deb9c
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-15 08:46:53 +0000

    Working on login form

---
+++ src/features/auth/login.jsx
@@ -0,0 +1,72 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useState } from 'react';
+import Switch from 'react-switch';
+import styles from './Auth.module.css';
+
+const logo = '/assets/alfredob.png';
+
+function Login() {
+  const [check, setCheck] = useState(false);
+
+  const initialState = {
+    email: '',
+    password: '',
+  };
+
+  const [credential, setCredential] = useState(initialState);
+
+  const handleInputChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    setCredential({ ...credential, [name]: value });
+  };
+
+  const handleCheckedChange = () => setCheck(!check);
+
+  const handleAuthSubmit = (event) => {
+    event.preventDefault();
+    console.log(credential);
+  };
+
+  return (
+    <div className={styles.loginContainer}>
+      <div>
+        <span>Light</span>
+        <Switch onChange={handleCheckedChange} checked={check} />
+        <span>Dark</span>
+      </div>
+      <div className="logo-container">
+        <img src={logo} alt="logo" className={styles.logoThumbnail} />
+        <div className="company-name">
+          <span>Air Pollution</span>
+          <span>portfolio analysis</span>
+        </div>
+        <div className="login-content">
+          <p className="login-cta">login with your dribble account</p>
+        </div>
+        <form action="#" method="post" onSubmit={handleAuthSubmit}>
+          <input
+            name="email"
+            placeholder="email"
+            value={credential.email}
+            type="email"
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            name="password"
+            placeholder="password"
+            value={credential.password}
+            onChange={handleInputChange}
+            type="password"
+            required
+          />
+
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import Home from './routes/home';
 import DetailsPage from './routes/details';
+import Login from './features/auth/login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Login />} />
       <Route path="/home" element={<Home />} />
       <Route path="/details" element={<DetailsPage />} />
     </Routes>
