e7714aeeca8f6dc82e25e6fee81d99b0638138cc
Alfred Boateng <kwadwo.boateng.alfred@gmail.com>
2022-07-25 23:41:02 +0100

    Add navbar

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   fetchCovidData,
 } from './features/covid/covidSlice';
 import Categories from './components/Categories';
+import Navbar from './components/navbar';
 // import CategoriesData from './Utils/CategoriesData';
 
 const App = () => {
@@ -37,17 +38,8 @@ const App = () => {
 
   return (
     <div>
+      <Navbar />
       <main className="container py-5">
-        <h1>24 latest news</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Necessitatibus, nobis. Quod quas tempore exercitationem modi excepturi
-          minima nemo ab sapiente molestiae at quisquam, nesciunt necessitatibus
-          distinctio illum, eveniet unde accusamus vitae dolorem sunt
-          laboriosam! Fuga architecto modi a minus at?
-        </p>
-
-        <hr className="my-5" />
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
           {renderCategories}
         </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,10 +3,84 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div>
-      <Link to="/">Home page</Link>
-      <Link to="/details">Details Page</Link>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          Covid 19 Tracker
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Highest Cases
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Lowest Cases
+              </Link>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="/"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Search Continent Data
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <button type="button" className="dropdown-item">
+                    Action
+                  </button>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <button type="button" className="dropdown-item">
+                    Action
+                  </button>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <button type="button" className="dropdown-item">
+                    Action
+                  </button>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
   );
 }
 export default Navbar;
