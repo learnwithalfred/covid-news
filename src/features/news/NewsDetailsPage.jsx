8b798dc440e557c2f77bf08f2e2afb3cf4d406fb
Alfred Boateng <kwadwo.boateng.alfred@gmail.com>
2022-07-25 12:11:50 +0100

    Update details page UI

--- src/features/news/NewsDetailsPage.jsx
+++ src/features/news/NewsDetailsPage.jsx
@@ -7,16 +7,44 @@ const NewsDetailsPage = () => {
 
   const renderNewsDetail = news.map((data) => (
     <div key={data.id}>
-      <img src={data.imageUrl} alt={data.author} />
-      <span>{data.time}</span>
-      <span>{data.date}</span>
-      <p>{data.content}</p>
+      <main>
+        <div className="container py-4">
+          <div className="row featurette">
+            <div className="col-md-6">
+              <h2 className="featurette-heading fw-normal lh-1">
+                {data.title}
+              </h2>
+              <span className="text-muted">
+                {data.date}
+                {' '}
+                |
+                {' '}
+                {data.time}
+                {' '}
+                by
+                {' '}
+                <a href={data.url} target="_blank" rel="noopener noreferrer">{data.author}</a>
+              </span>
+              <p className="lead mt-4">{data.content}</p>
+              <a className="mb-4 pb-4" target="blank" href={data.url}>Read more ...</a>
+            </div>
+            <div className="col-md-6">
+              <img
+                className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+                width={500}
+                height={500}
+                src={data.imageUrl}
+                alt={data.author}
+              />
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   ));
 
   return (
     <div>
-      <h2> NewsDetailsPage</h2>
       {renderNewsDetail}
     </div>
   );
