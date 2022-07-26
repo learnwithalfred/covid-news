065f7362a2d7935474018951c8cc721c86b28857
Alfred Boateng <kwadwo.boateng.alfred@gmail.com>
2022-07-26 07:46:39 +0100

    Clean up code

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import newsSlice from '../features/news/NewsSlice';
 import covidReducer from '../features/covid/covidSlice';
 
 const store = configureStore({
   reducer: {
-    news: newsSlice,
     covid: covidReducer,
   },
 });
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NewsDetailsPage from './features/news/NewsDetailsPage';
-import NewsCategoryPage from './features/news/NewsCategoryPage';
 import store from './app/store';
 
 import './index.css';
@@ -18,9 +16,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<App />} />
-        <Route exact path="/news/:category" element={<NewsCategoryPage />} />
         <Route exact path="/country/:name" element={<CovidDetailsPage />} />
-        <Route path="/news/:id/details" element={<NewsDetailsPage />} />
       </Routes>
     </BrowserRouter>
   </Provider>,
